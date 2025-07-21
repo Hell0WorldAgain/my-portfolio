e76dca0cd03223d242335fa10133fb2b83a75f44
Abhishek Choudhary <corporateabhishek102@gmail.com>
2025-07-21 22:39:44 +0530

    Fresh Files

---
+++ src/app/components/layout/header/Navigation/Menudata.tsx
@@ -0,0 +1,10 @@
+import { HeaderItem } from "../../../../types/menu";
+
+export const headerData: HeaderItem[] = [
+    { label: 'About us', href: '/#aboutus' },
+    { label: 'Services', href: '/#services' },
+    { label: 'Work', href: '/#work' },
+    { label: 'Team', href: '/#team' },
+    { label: 'Pricing', href: '/#pricing' },
+    { label: 'Awards', href: '/#awards' },
+];
