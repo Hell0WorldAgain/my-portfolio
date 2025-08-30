9dae2cb3eb9d1d3c34329960411273f0631cab5b
Abhishek <corporateabhishek47@gmail.com>
2025-08-31 02:55:01 +0530

    removed unnecessary files

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -1,16 +1,14 @@
 
 import ContactForm from "@/app/components/contact-form";
-import Faq from "@/app/components/home/faq";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-    title: "Contact | Awake Agency",
+    title: "Contact | Abhishek Choudhary",
 };
 
 export default function Page() {
     return (
         <main>
             <ContactForm/>
-            <Faq/>
         </main>
     );
 };
